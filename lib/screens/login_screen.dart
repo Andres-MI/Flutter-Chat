f9b3874f5435691d93619f768bc0709ff4387389
Andres Melenchon <andres.melenchon88@gmail.com>
2022-04-23 09:56:06 +0200

    Refactor. Extract Button Widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../components/rounded_button.dart';
+
+
 class LoginScreen extends StatefulWidget {
 
   static const String screenId = 'LoginScreenId';
@@ -81,24 +84,9 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(buttonColor: Colors.lightBlueAccent, buttonText: 'Log In', onPressed: () {
+              //Implement Log in functionality
+            }),
           ],
         ),
       ),
